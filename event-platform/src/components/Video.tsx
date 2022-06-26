b19338de64435bef7dba1c9943798daa87b20f99
alissonfernandes <alisson.flutter@gmail.com.br>
2022-06-26 10:32:11 -0300

    aula 5: deploy, codegen e proximos passos

--- event-platform/src/components/Video.tsx
+++ event-platform/src/components/Video.tsx
@@ -3,45 +3,18 @@ import { gql, useQuery } from "@apollo/client";
 
 import '@vime/core/themes/default.css';
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug($slug: String) {
-    lesson(where: {slug: $slug}) {
-      title
-      videoId
-      description
-      teacher {
-        bio
-        avatarURL
-        name
-      }
-    }
-  }
-`
-
-interface GetLessonBySlugResponse {
-  lesson: {
-    title: string;
-    videoId: string;
-    description: string;
-    teacher: {
-      bio: string;
-      avatarURL: string;
-      name: string;
-    }
-  }
-}
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
 interface VideoProps {
   lessonSlug: string;
 }
 
 export function Video(props: VideoProps) {
-  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+  const { data } = useGetLessonBySlugQuery({
     variables: {
       slug: props.lessonSlug
     }
-  });
+  })
 
   if (!data) {
     return (
@@ -57,7 +30,7 @@ export function Video(props: VideoProps) {
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
           <Player>
-            <Youtube videoId={data.lesson.videoId} />
+            <Youtube videoId={data.lesson!.videoId} />
             <DefaultUi />
           </Player>
         </div>
@@ -67,26 +40,26 @@ export function Video(props: VideoProps) {
         <div className="flex items-start gap-16">
           <div className="flex-1">
             <h1 className="text-2xl font-bold">
-              {data.lesson.title}
+              {data.lesson!.title}
             </h1>
 
             <p className="mt-4 text-gray-200 leading-relaxed">
-              {data.lesson.description}
+              {data.lesson!.description}
             </p>
 
             <div className="flex items-center gap-4 mt-6">
               <img
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src={data.lesson.teacher.avatarURL}
+                src={data.lesson!.teacher!.avatarURL}
                 alt=""
               />
 
               <div className="leading-relaxed">
                 <strong className="font-bold text-2xl block">
-                  {data.lesson.teacher.name}
+                  {data.lesson!.teacher!.name}
                 </strong>
                 <span className="text-sm text-gray-200 block">
-                  {data.lesson.teacher.bio}
+                  {data.lesson!.teacher!.bio}
                 </span>
               </div>
             </div>
